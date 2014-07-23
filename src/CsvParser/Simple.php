10b1991e71d55c2c867a10896ff80f64daad31a1
Christian Bolstad <christian@carnaby.se>
2014-07-23 16:28:06 +0200

    throw Exception if parameters is missing

--- src/CsvParser/Simple.php
+++ src/CsvParser/Simple.php
@@ -1,9 +1,11 @@
-<?php 
+<?php
 namespace CsvParser;
 
 class Simple {
-	public function parseRowByRow() {
-	echo "hi\n";
-}
+	public function parseRowByRow( $filname = '', $callback = '' ) {
+		if (empty($filname) || empty($callback)) 
+	        throw new \Exception('Missing parameters');
+		echo "hi\n";
+	}
 
 }
